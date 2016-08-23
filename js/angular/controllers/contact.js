9cb9654729f13a67b3c57813a38261039370fadb
Mohamed.Ghannem <mghannem@fr.aareon.local>
2016-08-23 15:50:19 +0100

    contact commit

--- js/angular/controllers/contact.js
+++ js/angular/controllers/contact.js
@@ -1,2 +1,22 @@
-myApp.controller('contactCtr',['$scope', function ($scope) {
+myApp.controller('contactCtr',['$scope', '$http', function ($scope, $http) {
+
+  $scope.submit = function() {
+    try {
+        if($scope.formContact.$valid){
+          $http(
+            {
+              method : 'POST',
+              url: '/toto.php',
+              data: [$scope.email,$scope.message,$scope.name, $scope.raison]
+            }
+          ).then(
+            function ok(response){console.log('post is ok :' + response.data)},
+            function ko(response){console.log('post error'+response.status)});
+        } else {
+          console.log('form is invalid');
+        }
+    } catch (err) {
+      console.log(err.name);
+    }
+  }
 }]);
